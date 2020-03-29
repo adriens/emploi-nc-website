289e35f23d3d45163113e7075a08a754df966e73
AVAE Jimmy (EXT) <JAVAE@intranet.opt>
2020-03-30 10:53:05 +1100

    feat(Implement Materialize)
    Replace hugo theme universal by a new theme using materialize
    
    Fixes #2

---
+++ static/js/initialisation.js
@@ -0,0 +1,50 @@
+var carousel = jQuery('div#home-carousel');
+  carousel.carousel({
+    fullWidth: true,
+    indicators: true,
+    duration: 300,
+    onCycleTo : function($current_item, dragged) {
+      console.log($current_item);
+      stopAutoplay();
+      startAutoplay(carousel);
+    }
+  }
+);
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true,
+});
+
+
+function goToslide(i){
+  $('.carousel.carousel-slider').carousel('set',i);
+}
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  $('.tabs').tabs();
+  $('.tap-target').tapTarget();
+  $('.collapsible').collapsible();
+  $('.parallax').parallax();
+  $('.tap-target').tapTarget('open');
+  $('.dropdown-trigger').dropdown();
+});
+
+
+
+var autoplay_id;
+function startAutoplay($carousel) {
+   autoplay_id = setInterval(function() {
+      $carousel.carousel('next');
+    }, 8000); // every 5 seconds
+  //console.log("starting autoplay");
+}
+
+function stopAutoplay() {
+  if(autoplay_id) {
+    clearInterval(autoplay_id);
+    //console.log("stoping autoplay");
+  }
+}
+
