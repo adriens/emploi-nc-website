093931f1de8a64f09e82c51d06f590bbdb09e8b8
AVAE Jimmy <JAVAE@opt.nc>
2020-05-04 10:40:57 +1100

    feat(smooth scroll)
    Fixes #7

--- public/js/initialisation.js
+++ public/js/initialisation.js
@@ -4,7 +4,6 @@ var carousel = jQuery('div#home-carousel');
     indicators: true,
     duration: 300,
     onCycleTo : function($current_item, dragged) {
-      console.log($current_item);
       stopAutoplay();
       startAutoplay(carousel);
     }
@@ -38,13 +37,11 @@ function startAutoplay($carousel) {
    autoplay_id = setInterval(function() {
       $carousel.carousel('next');
     }, 8000); // every 5 seconds
-  //console.log("starting autoplay");
 }
 
 function stopAutoplay() {
   if(autoplay_id) {
     clearInterval(autoplay_id);
-    //console.log("stoping autoplay");
   }
 }
 
